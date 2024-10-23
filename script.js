6529459d348d037e91c584e6406389c9a218fda2
Minindu <mininduthiranjaya50@gmail.com>
2024-10-23 14:54:10 +0530

    add close icon

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click",()=>{
     swal("Hello!", "You clicked the login button!", "success");
 })
+
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+    const barsIcon = this.querySelector('.fa-bars');
+    const timesIcon = this.querySelector('.fa-times');
+    
+    // Toggle the 'd-none' class to show or hide the icons
+    barsIcon.classList.toggle('d-none');
+    timesIcon.classList.toggle('d-none');
+});
